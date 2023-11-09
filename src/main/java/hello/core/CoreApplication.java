12ecf1f1c30421789d081b6a3a4bf3ae91d18def
KwonJuyeong <20161527@vision.hoseo.edu>
2023-11-09 11:57:34 +0900

    Spring 공부 1

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoreApplication {
-
+	//스터디
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
 	}
