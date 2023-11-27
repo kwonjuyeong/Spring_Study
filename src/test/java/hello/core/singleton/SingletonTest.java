4be7ab6cd37c7b50ed65b2b5e6acab1271d1d124
KwonJuyeong <20161527@vision.hoseo.edu>
2023-11-27 15:29:03 +0900

    Singleton패턴 구현

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -24,4 +24,19 @@ public class SingletonTest {
         assertThat(memberService1).isNotSameAs(memberService2);
 
     }
+
+    //싱글톤 패턴 구현, 인스턴스가 같음.
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+
 }
